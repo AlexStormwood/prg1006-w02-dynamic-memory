7f5c546fc5540181370c2ecd749edb3e0f229840
Alex <alex.stormwood@bigfootds.com>
2026-06-04 15:10:59 +1000

    feat: shopping list start

--- prg1006-w02-dynamic-memory.cpp
+++ prg1006-w02-dynamic-memory.cpp
@@ -1,54 +1,55 @@
-// prg1006-w02-dynamic-memory.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-    // claim memory on free store  for this string 
-    std::string* message = new std::string("Hello World!");
+	std::cout << "Welcome to the shop!" << std::endl;
 
-    std::cout << message << std::endl;
+	// set a hard limit of the number of items that a user can enter 
+	const int maximumItems = 10;
 
-	// release memory back to free store
-    delete message;
-    // specifically set the pointer back to null so we don't
-	// accidentally use it again and cause a runtime error
-    message = nullptr;
-	// confirm that the pointer is null
-    std::cout << message;
+	// set space in memory for the user's shopping list items
+	std::string* shoppingList = new std::string[maximumItems];
+	// REMEMBER: if you use "new", make sure to use "delete" at some point later!!!
+
+	// keep track of how many items the user has entered so far
+	int itemCount = 0;
+
+	// Receive user input and either store it in the shopping list
+	// or check if the user said "finished" to end the program
+	std::string userInput;
+
+	// loop until the user says "finished" or enters 10 items 
+	while (itemCount < maximumItems) {
+		std::cout << "Enter an item into the shopping list or type 'finished' to end the program" << std::endl;
+		std::getline(std::cin, userInput);
+
+		if (userInput == "finished") {
+			// end the program
+			break;
+		}
+		else {
+			shoppingList[itemCount] = userInput;
+			// make sure we increment this to make sure 
+			// the while loop eventually ends AND 
+			// that we don't overwrite existing shopping list items!
+			itemCount++;
+		}
+
+	}
 
 
-    // nothrow helps the app run even after something would've broken
-	// its memory allocation, e.g. bad user input, or a memory leak, etc.
-    // (std::nothrow)
-    try {
-        int* someNumberPointer = new int[10000000000000];
-        std::cout << someNumberPointer << std::endl;
-        if (someNumberPointer == nullptr) {
-            std::cout << "Memory allocation failed!" << std::endl;
-        }
-    }
-    catch (const std::bad_alloc& error) {
-        std::cout << "Memory allocation failed: " << error.what() << std::endl;
-        // we should return 1 here to close the app, but......
-        return 1;
-    }
-	
-    std::cout << "See if this message appears after the catch message!" << std::endl;
+	// display the shopping list to the user 
+	// 1. Bananas
+	for (int shoppingListIndex = 0; shoppingListIndex < itemCount; shoppingListIndex++) {
+		// make sure to do "shoppingListIndex + 1" to display the human-friendly list numbering
+		std::cout << shoppingListIndex + 1 << ". " << shoppingList[shoppingListIndex] << std::endl;
+	}
 
+	// delete/release any claimed memory
+	delete shoppingList;
 
-
+	return 0;
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
